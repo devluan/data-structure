aa850c588d4df2e44ce8ad8f9627fbe07961d10d
Luan Ferreira <181789192+devluan@users.noreply.github.com>
2026-02-08 11:52:07 -0300

    Initial commit

---
+++ src/search/search.c
@@ -0,0 +1,24 @@
+#include "search.h"
+
+int sequencial_search(int arr[], int length, int target) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == target) return i;
+    }
+
+    return -1;
+}
+
+int binary_search(int arr[], int length, int target) {
+    int start = 0;
+    int end = length - 1;
+    int mid;
+
+    while (start <= end) {
+        mid = start + (end - start) / 2;
+        if (arr[mid] == target) return mid;
+        if (arr[mid] < target) end = mid - 1;
+        else start = mid + 1;
+    }
+    
+    return -1;
+}
---
+++ src/search/search.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+int sequencial_search(int arr[], int length, int target);
+
+int binary_search(int arr[], int length, int target);
+
+#endif

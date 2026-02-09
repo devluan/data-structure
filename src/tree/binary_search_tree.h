b88898e3322f90a2988e20a4814118627cbe13af
Luan Ferreira <181789192+devluan@users.noreply.github.com>
2026-02-09 17:11:46 -0300

    Adiciona Binary Search Tree

---
+++ src/tree/binary_search_tree.h
@@ -0,0 +1,32 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include <stdbool.h>
+
+typedef struct Node Node;
+
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+};
+
+typedef struct BinarySearchTree BinarySearchTree;
+
+struct BinarySearchTree {
+    Node *root;
+    void (*insert)(BinarySearchTree *self, int key);
+    void (*in_order_traverse)(BinarySearchTree *self, void (*callback)(int key));
+    void (*pre_order_traverse)(BinarySearchTree *self, void (*callback)(int key));
+    void (*post_order_traverse)(BinarySearchTree *self, void (*callback)(int key));
+    Node *(*min)(BinarySearchTree *self);
+    Node *(*max)(BinarySearchTree *self);
+    bool (*search)(BinarySearchTree *self, int key);
+    void (*remove)(BinarySearchTree **self, int key);
+};
+
+BinarySearchTree *new_binary_search_tree();
+
+void free_binary_search_tree(BinarySearchTree **tree);
+
+#endif
